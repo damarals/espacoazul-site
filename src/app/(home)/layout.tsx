0a5d599a1196f445e756e8c1a9c433b9cf1140fb
Daniel Silva <silva.daniel86@gmail.com>
2024-01-08 00:10:33 -0300

    🐛 fix(our kids): fix text sizes

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { currentUser } from '@/lib/auth'
 import ContactForm from '@/components/ContactForm'
@@ -32,16 +33,23 @@ export default async function MainLayout({ children }: MainLayoutProps) {
         >
           {/* Our Kids */}
           <div className="flex h-screen flex-col items-center gap-10 pb-12 pt-24">
-            <h1 className="max-w-4xl text-center font-serif text-5xl font-semibold text-primary-dark">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry
+            <h1 className="max-w-4xl px-4 text-center font-serif text-5xl font-semibold text-primary-dark">
+              Nossas Crianças
             </h1>
             <ImageGallery />
-            <p className="max-w-4xl text-center text-lg font-normal text-primary-dark">
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia.
+            <p className="max-w-4xl px-4 text-center text-lg font-normal text-primary-dark">
+              <span className="hidden max-w-4xl text-center text-lg font-normal text-primary-dark sm:inline-block">
+                Compartilhe momentos de sua criança conosco!
+              </span>{' '}
+              Se seu filho(a) faz parte do Projeto Espaço Azul, você pode
+              incluir a foto dele(a) acima. Preencha o formulário através deste{' '}
+              <Link href="" className="font-medium underline">
+                link
+              </Link>
+              .{' '}
+              <span className="hidden max-w-4xl text-center text-lg font-normal text-primary-dark sm:inline-block">
+                Juntos, celebramos a alegria e a beleza de cada jornada única.
+              </span>
             </p>
           </div>
           {/* Posts & News */}
