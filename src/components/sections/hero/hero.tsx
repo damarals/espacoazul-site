cf03732beac77ab577cf66202d117b729f654099
Daniel Silva <silva.daniel86@gmail.com>
2024-03-06 15:18:53 -0300

    ✨ feat(hero): add text to hero

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -12,60 +12,66 @@ export default function Hero() {
         {/* Info */}
         <div className="relative z-20 flex w-full flex-1 flex-col items-center justify-center gap-9 text-center text-primary-dark sm:gap-24 md:items-start md:gap-16 md:text-left lg:gap-24 lg:pr-10">
           {/* Text */}
-          <div className="flex flex-col gap-3 sm:gap-4">
-            <h3 className="hidden text-xl font-bold uppercase tracking-[0.45em] md:block 2xl:text-2xl">
-              Sobre nós
-            </h3>
-            <div className="flex flex-col gap-5 sm:gap-10">
-              <h1 className="max-w-2xl font-serif text-6xl font-extrabold sm:text-7xl md:text-6xl xl:text-7xl 2xl:text-8xl">
-                Lorem ipsum dolor sit amet.
-              </h1>
-              <h2 className="text-lg font-normal sm:text-xl md:max-w-2xl md:text-lg lg:text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-                aliquam erat aliquam erat volutpat.
-              </h2>
-              <div className="flex flex-col items-center gap-4 md:flex-row">
-                <Link
-                  href="/#contato"
-                  className={cn(
-                    buttonVariants(),
-                    'rounded-full bg-blue px-6 py-6 text-base font-extrabold text-white lg:px-9 lg:text-lg',
-                  )}
-                >
-                  Entre em contato
-                </Link>
-                <span className="hidden text-base text-primary-dark xl:block xl:text-lg">
-                  ou
-                </span>
-                <Link
-                  href="/institucional"
-                  className={cn(
-                    buttonVariants(),
-                    'rounded-full bg-white px-6 py-6 text-base font-extrabold text-primary-dark lg:px-9 lg:text-lg',
-                  )}
-                >
-                  Saiba mais
-                </Link>
-              </div>
+          <div className="flex flex-col gap-5 sm:gap-10">
+            <h1 className="max-w-2xl font-serif text-6xl font-extrabold sm:text-7xl md:text-6xl xl:text-7xl 2xl:text-8xl">
+              Reabilitando vidas autistas.
+            </h1>
+            <h2 className="hidden text-lg font-normal sm:block sm:text-xl md:max-w-2xl md:text-lg lg:text-xl">
+              Construindo uma comunidade solidária, nossos profissionais
+              voluntários oferecem suporte holístico, abrangendo desde a
+              compreensão até serviços especializados para autistas e suas
+              famílias.
+            </h2>
+            <h2 className="text-lg font-normal sm:hidden sm:text-xl md:max-w-2xl md:text-lg lg:text-xl">
+              Construindo uma comunidade solidária, oferecemos suporte holístico
+              e serviços especializados para autistas e suas famílias.
+            </h2>
+            <div className="flex flex-col items-center gap-4 md:flex-row">
+              <Link
+                href="/#contato"
+                className={cn(
+                  buttonVariants(),
+                  'rounded-full bg-blue px-6 py-6 text-base font-extrabold text-white lg:px-9 lg:text-lg',
+                )}
+              >
+                Entre em contato
+              </Link>
+              <span className="hidden text-base text-primary-dark xl:block xl:text-lg">
+                ou
+              </span>
+              <Link
+                href="/institucional"
+                className={cn(
+                  buttonVariants(),
+                  'rounded-full bg-white px-6 py-6 text-base font-extrabold text-primary-dark lg:px-9 lg:text-lg',
+                )}
+              >
+                Saiba mais
+              </Link>
             </div>
           </div>
           {/* Stats */}
           <div className="flex w-full max-w-[300px] flex-row flex-wrap justify-between gap-4 sm:max-w-[460px] md:flex-col lg:max-w-[440px] xl:flex-row xl:gap-5 2xl:max-w-[510px]">
             <div className="flex flex-col items-center gap-1 md:flex-row md:items-end md:gap-2 xl:flex-col xl:items-start xl:gap-1">
-              <span className="text-2xl font-extrabold md:text-2xl">+7</span>
+              <span className="text-2xl font-extrabold md:text-2xl xl:text-3xl">
+                +7
+              </span>
               <span className="pb-[0] text-lg font-medium md:pb-[1px] md:text-base xl:pb-0 xl:text-lg">
                 Anos de atuação
               </span>
             </div>
             <div className="hidden flex-col items-center gap-1 sm:flex md:flex-row md:items-end md:gap-2 xl:flex-col xl:items-start xl:gap-1">
-              <span className="text-2xl font-extrabold md:text-2xl">+1000</span>
+              <span className="text-2xl font-extrabold md:text-2xl xl:text-3xl">
+                +1000
+              </span>
               <span className="pb-[0] text-lg font-medium md:pb-[1px] md:text-base xl:pb-0 xl:text-lg">
                 Crianças atendidas
               </span>
             </div>
             <div className="flex flex-col items-center gap-1 md:flex-row md:items-end md:gap-2 xl:flex-col xl:items-start xl:gap-1">
-              <span className="text-2xl font-extrabold md:text-2xl">+100</span>
+              <span className="text-2xl font-extrabold md:text-2xl xl:text-3xl">
+                +100
+              </span>
               <span className="pb-[0] text-lg font-medium md:pb-[1px] md:text-base xl:pb-0 xl:text-lg">
                 Voluntários
               </span>
